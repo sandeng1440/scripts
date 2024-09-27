7cb7cfcfcfec995324c1775c78bac7a52497823f
sandeng1440 <sandeng1440@gmail.com>
2024-09-27 13:14:00 +0300

    Gitup commit

--- user.nix
+++ user.nix
@@ -1,7 +1,16 @@
 with import <nixpkgs> {}; [
 	# essentials
-	janet jq direnv nix-direnv
+	janet jq direnv nix-direnv cachix
 
 	# security
 	bettercap
+
+	# office
+	#libreoffice-fresh
+
+	# development
+	android-studio
+
+	# games
+	#mindustry-wayland
 ]
